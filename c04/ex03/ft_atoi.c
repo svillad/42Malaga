44d0fe79af00f20fa0ace72cce33deee3c28916a
sebastianvilla <sebastian.villaduarte@imaginecurve.com>
2023-07-13 21:28:58 +0200

    C[04] ex03

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -1,66 +1,95 @@
-typedef enum {
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svilla-d <svilla-d@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/13 15:25:22 by svilla-d          #+#    #+#             */
+/*   Updated: 2023/07/13 21:26:04 by svilla-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+typedef enum _estados {
 	E_INICIAL,
 	E_ESPACIOS,
 	E_SIGNOS,
 	E_NUMEROS,
-	E_OTROS
-} Estados;
+	E_FIN
+}	t_estados;
 
+int	ft_char_in_str(char c, const char *str);
 int	ft_atoi(char *str);
-int	ft_strlen(char *str);
 
-int	ft_strlen(char *str)
+int	ft_char_in_str(char c, const char *str)
 {
-	int	i;
+	int			i;
 
 	i = 0;
-	while (*(str+i))
+	while (str[i])
+	{
+		if (c == str[i])
+			return (1);
 		i++;
-	return (i);
+	}
+	return (0);
 }
 
-int	ft_is_space(char c){
-	int i;
+t_estados	ft_get_state(t_estados estado, char c, int *num, int *signo)
+{
 	const char	spaces[] = " \n\v\t\r\f";
+	const char	signs[] = "+-";
+	const char	numbers[] = "0123456789";
 
-	int i = 0;
-	while (spaces[i])
+	if (ft_char_in_str(c, spaces)
+		&& (estado == E_INICIAL || estado == E_ESPACIOS))
+		estado = E_ESPACIOS;
+	else if (ft_char_in_str(c, signs)
+		&& (estado == E_INICIAL || estado == E_ESPACIOS || estado == E_SIGNOS))
 	{
-		if (c == spaces[i])
-			return (1);
-		i++;
+		estado = E_SIGNOS;
+		if (c == '-')
+			*signo = *signo * -1;
 	}
-	return (0);
-}
-
-int	ft_is_sign(char c){
-	int i;
-	const char	sign[] = "+-";
-
-	int i = 0;
-	while (sign[i])
+	else if (ft_char_in_str(c, numbers))
 	{
-		if (c == sign[i])
-			return (1);
-		i++;
+		estado = E_NUMEROS;
+		*num = *num * 10 + (c - 48);
 	}
-	return (0);
+	else
+		estado = E_FIN; 
+	return (estado);
 }
 
 int	ft_atoi(char *str)
 {
-	int	len;
-	Estados control;
+	int			i;
+	int			num;
+	int			signo;
+	t_estados	estado;
 
-	len = ft_strlen(str);
-	control = E_INICIAL;
-	while (control != E_OTROS){
-		if (ft_is_space(*str))
-			control = ESTADO_ESPACIOS;
-		else if (ft_is_sign(*str))
-			control = ESTADO_SIGNOS;
-		else if ()
+	estado = E_ESPACIOS;
+	i = 0;
+	signo = 1;
+	num = 0;
+	while (estado != E_FIN && str[i])
+	{
+		estado = ft_get_state(estado, str[i], &num, &signo);
+		i++;
 	}
-	return (i);
+	num = num * signo;
+	return (num);
 }
 
+// int main() {
+// 	printf("TEST\n");
+// 	printf("%i\n", ft_atoi("\t\n\v\f\r      23"));
+// 	printf("%i\n", ft_atoi("     ------+++++---12341"));
+// 	printf("%i\n", ft_atoi("     ------+++++---123413"));
+// 	printf("%i\n", ft_atoi("00000000002147483647"));
+// 	printf("%i\n", ft_atoi("-2147483648"));
+// 	printf("%i\n", ft_atoi("a"));
+// 	return (0);
+// }
