99cc94ff26b6cce39b3c4bea5b38b9acceed0a27
Sebastian Villa Duarte <svilla-d@c1r10s6.42malaga.com>
2023-07-20 11:25:29 +0200

    [C05] ex08

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
-#define N 10
+#define N 4
+
+char  *ft_get_queens_positions(int **table);
+int	**ft_init_table(int n);
+int ft_is_good_position(int **table, int fila, int col);
+int ft_ten_queens_puzzle(void);
+
+char  *ft_get_queens_positions(int **table)
+{
+	int		i;
+	int		j;
+	int 	p;
+	char	*queens;
+
+	queens = (char *)malloc ((N+2) * sizeof(char));
+	i = -1;
+	p = 0;
+	while (++i < N)
+	{
+		j = -1;
+		while (++j < N)
+		{
+			if (table[i][j] == 1)
+			{
+				queens[p] = j + '0';
+				p++;
+				break ;
+			}
+		}	
+	}
+	queens[p++] = '\n';
+	queens[p] = '\0';
+	return queens;
+}
 
 void ft_print_table(int **table) {
 	for (int i = 0; i < N; i++) {
@@ -57,15 +91,26 @@ int ft_is_good_position(int **table, int fila, int col)
 }
 
 int ft_set_queens(int **table, int fila) {
-	int col;
-	int soluciones = 0;
+	int		col;
+	int		soluciones;
+	char	*queens;
+	char	c;
 
 	if (fila == N){
-		ft_print_table(table);
+		// ft_print_table(table);
+		queens = ft_get_queens_positions(table);
+		write(1, queens, N+1);
+		// scanf("%c", &c);
+		free(queens);
 		return (1);
 	}
+
+	soluciones = 0;
 	col = -1;
 	while (++col < N) {
+		ft_print_table(table);
+		scanf("%c", &c);
+
 		if (ft_is_good_position(table, fila, col)) {
 			table[fila][col] = 1;
 			soluciones += ft_set_queens(table, fila + 1);
@@ -75,16 +120,16 @@ int ft_set_queens(int **table, int fila) {
 	return soluciones;
 }
 
-int ft_ten_queens_puzzle(void);
-
 int ft_ten_queens_puzzle(void) {
-	int **table;
-	int soluciones; 
+	int		**table;
+	int		soluciones; 
 
 	table = ft_init_table(N);
-	ft_print_table(table);
+	// ft_print_table(table);
 	soluciones = ft_set_queens(table, 0);
-
+	//soluciones = ft_ten_queens_puzzle();
+	printf("Total de soluciones encontradas: %d\n", soluciones);
+	free(table);
 	return soluciones;
 }
 
