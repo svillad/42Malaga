dd078f6f06138bb6507aa0dce709d521e497c7dc
svillad <svillad@gmail.com>
2025-02-22 16:41:42 +0100

    [CPP_06] Added 42 header

--- cursi/ring_5/cpp_06/ex02/main.cpp
+++ cursi/ring_5/cpp_06/ex02/main.cpp
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svilla-d <svilla-d@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 16:39:32 by svilla-d          #+#    #+#             */
+/*   Updated: 2025/02/22 16:39:33 by svilla-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Functions.hpp"
 #include <iostream>
 #include <ctime>
 
 int main() {
+    srand(time(NULL));
     std::cout << "\n---------- Generating Random Base Objects ----------\n" << std::endl;
     for (int i = 0; i < 5; i++) {
         std::cout << "* Creating instance" << std::endl;
